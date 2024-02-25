00a728b530a0b3d62448b3753ae948084e36a213
Hammad Raza Khan <hammadrazakhann@gmail.com>
2024-02-25 16:19:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+    
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default= datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+    
+    
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    if  request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content= task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            return 'There was an issue adding your Task !'
+    
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    tasktodelete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(tasktodelete)
+        db.session.commit()
+        return redirect('/')
+        
+    except:
+        return 'Error in deleting task'
+    
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+    
+        except:
+            return "Error updating your task"
+    
+    
+    else:
+        return render_template('update.html', task = task)
+
+if __name__ == "__main__":
+     with app.app_context():
+        db.create_all()
+        app.run(debug=True)
